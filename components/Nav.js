372cc9ad4474ee217df44987fca4b01cd663b965
fsmutimeer <ishpata@hotmail.com>
2022-01-29 20:18:40 +0500

    ..

--- components/Nav.js
+++ components/Nav.js
@@ -4,12 +4,13 @@ import Link from 'next/link';
 
 const Nav = () => {
   return <div>
-      <ul>
+      <ul >
           <li><Link href='/'><a>Home</a></Link></li>
           <li><Link href='/about'><a>About</a></Link></li>
           <li><Link href='/contact'><a>Contact</a></Link></li>
       </ul>
   </div>;
 };
+
 
 export default Nav;
