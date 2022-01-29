3ac8cfe500f4bcecc7409de6071d80b5d52c0cfe
fsmutimeer <ishpata@hotmail.com>
2022-01-29 18:40:12 +0500

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const about = ()=>{
+    return <>
+    About Me
+    <Link href='/'>Go to Home</Link>
+    </>
+}
+
+export default about;
---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const contact = () => {
+    return <>
+    About Me
+    <Link href='/'>Go to Home</Link>
+    </>
+};
+
+export default contact;
